4efc990001d057a9c64ae01f228a5fb50f30a047
Varsha Umesh <varsha.bu@ymedialabs.com>
2022-03-03 14:26:10 +0530

    add document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Next-auth Example app</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
